1cb0299e75d2e71ebc0146e5e5d9655fa7ae3c30
ROHIT SHARMA <41463472+rohitjsarma@users.noreply.github.com>
2021-03-23 00:31:33 +0530

    reactWithBootstrap

---
+++ 03-react-with-bootstrap/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Cards from "./components/Cards";
+
+function App() {
+  return (
+    <div className="App">
+        <nav>
+            <a href="/">React JS Cards with Styles</a>
+        </nav>
+
+        <Cards/>
+
+    </div>
+  );
+}
+
+export default App;
